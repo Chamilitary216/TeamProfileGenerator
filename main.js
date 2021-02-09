c9d833bf21c5f3133d364a6b8394c17b7437f0e8
Chris Curry <Chamilitary216@hotmail.com>
2021-02-09 01:31:06 -0500

    started building html template

--- main.js
+++ main.js
@@ -69,5 +69,21 @@ function addMember () {
     })
 
 }
+// HTML template
+function combineModules(Naruto){
+    console.log(employees)
+    writeToFile("./Assest/index.html",
+    '<!DOCTYPE html>
+    <html lang = "en-us">
+
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Team Generator </title>
+    </head>
+</html> 
+
+}
 
 initApp();
